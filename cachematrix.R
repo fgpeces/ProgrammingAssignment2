6f89f60832fbfac45394b7dc0fa6f03cf6af9498
fgpeces <javier.gpeces@gmail.com>
2014-05-19 11:36:52 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##This function creates a special"matrix", which is really a list containing a function to
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the inverse
+##get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {     
         s <- null
         set <- function(y) {
@@ -19,11 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+##Calculate the inverse of the special "matrix" returned by above function. If the inverse has already been calculated and 
+##matriz has not changed retrieve the inverse from the cache, otherwise calculate the inverse of the data and sets 
+##the value of the inverse via setsolve function
+##To check if the matriz has not changed we check that the AxA*=I that should be the same of (AXA*)*=I*=I
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         data <- x$get()
         s <- x$getsolve()
-        if(!is.null(s)){
+        if(!is.null(s) && identical(data %*% s,solve(data %*% s))){
                 message("getting cached data")
                 return(s)
         }
